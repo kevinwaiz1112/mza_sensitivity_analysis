e89526cde6394e6cacb7dcc04f17bbb130a6079c
KevinWaiz <kevin.waiz@dlr.de>
2026-03-02 21:02:00 +0100

    new license paths

--- run_remote.sh
+++ run_remote.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 D=/opt/dymola-2026x-x86_64
 export DYMOLA="$D"
 export LM_LICENSE_FILE=27000@kpfiler4.intra.dlr.de
+export DSLS_LICENSE_FILE="$LM_LICENSE_FILE"
+export DYMOLA_LICENSE_FILE="$LM_LICENSE_FILE"
 export QT_QPA_PLATFORM=offscreen
 export LD_LIBRARY_PATH="$D/bin/lib64:$D/bin/lib:$D/bin64:${LD_LIBRARY_PATH:-}"
 export DYMOLA_USERHOME="$HOME/.dassaultsystemes/dymola/2026x"
